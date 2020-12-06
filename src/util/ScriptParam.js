00e9b5ab3c05c83cac7e6f3053a8dc57e1a4c799
Ruben <ruben_dal@hotmail.com>
2020-12-06 09:53:28 -0600

    Hitbox param descriptions

--- src/util/ScriptParam.js
+++ src/util/ScriptParam.js
@@ -75,18 +75,18 @@ function GetDescriptionForParam(paramName, value) {
             </span>);
         case "BKB":
             return (<span>
-                Base KnockBack,<br />
-              minimum amount of knockback done regardless of damage and percent
+                Base KnockBack<br />
+              Minimum amount of knockback done regardless of damage and percent
             </span>);
         case "FKB":
             return (<span>
-                Fixed KnockBack,<br />
-              previously known as WBKB (Weight Based KnockBack), if it's not 0 the knockback formula uses this value as opponent's percent and ignores move damage, these moves will deal the same knockback regardless of their damage and percentage the opponent has but still varies by opponent's weight
+                Fixed KnockBack<br />
+              Previously known as WBKB (Weight Based KnockBack), if it's not 0 the knockback formula uses this value as opponent's percent and ignores move damage, these moves will deal the same knockback regardless of their damage and percentage the opponent has but still varies by opponent's weight
             </span>);
         case "KBG":
             return (<span>
                 KnockBack Growth,<br />
-              in the knockback formula describes how much damage and percent scales
+              In the knockback formula describes how much damage and percent scales
             </span>);
         case "Size":
             return "Size of the hitbox";
@@ -102,22 +102,34 @@ function GetDescriptionForParam(paramName, value) {
         case "Z2":
             return "Stretch coordinate for extended hitboxes";
         case "Hitlag":
-            return "Multiplier for HitLag";
+            return "Multiplier for Hitlag";
         case "SDI":
             return (<span>
-                Multiplier for Smash Directional Influence,<br />
-              easy to SDI if the value is larger than 1.0
+                Multiplier for Smash Directional Influence distance
             </span>);
         case "Clang_Rebound":
-            return "no description";
-        case "FacingRestrict":
             candidateList = [
-                ["ATTACK_LR_CHECK_POS", "no description"],
-                ["ATTACK_LR_CHECK_F", "no description"],
-                ["ATTACK_LR_CHECK_B", "no description"],
+                ["ATTACK_SETOFF_KIND_OFF", "Doesn't interact (transcendent)"],
+                ["ATTACK_SETOFF_KIND_ON", "Interacts with other hitboxes by clanking, causes rebound if hitbox doesn't deal more than 9% more damage than opposing hitbox"],
+                ["ATTACK_SETOFF_KIND_THRU", "Interacts with other hitboxes by clanking, doesn't cause rebound"],
+                ["ATTACK_SETOFF_KIND_NO_STOP", "Interacts with other hitboxes but doesn't stop being active, unique to Mii Gunner Grenade Launch"]
             ]
             return (<span>
-                Possible values are:
+                Indicates how hitbox interacts with other hitboxes
+                {DescribeCandidateList(candidateList, value)}
+            </span>);
+        case "FacingRestrict":
+            candidateList = [
+                ["ATTACK_LR_CHECK_POS", "Launch direction is determined by character's Trans bone position"],
+                ["ATTACK_LR_CHECK_F", "Launch direction will be the same as the direction this character front is facing"],
+                ["ATTACK_LR_CHECK_B", "Launch direction will be the same as the direction this character back is facing"],
+                ["ATTACK_LR_CHECK_SPEED", "Launch direction is based on the character/projectile direction it's moving"],
+                ["ATTACK_LR_CHECK_PART","Launch direction is determined by hitbox bone position"],
+                ["ATTACK_LR_CHECK_LEFT","Replaces angle to launch where it originated, used on Rosalina's Final Smash small stars"],
+                ["ATTACK_LR_CHECK_BACK_SLASH","Calculates launch angle using opponent's back direction, used in Shulk's Backslash"]
+            ]
+            return (<span>
+                Indicates how launch direction and opponent facing direction be when hit
                 {DescribeCandidateList(candidateList, value)}
             </span>);
         case "SetWeight":
@@ -137,34 +149,69 @@ function GetDescriptionForParam(paramName, value) {
         case "DisableHitlag":
             return "Flag that makes hitlag = 0 regardless of damage and hitlag multipliers when enabled";
         case "Direct_Hitbox":
-            return "no description";
+            return "Flag that indicates if hitbox isn't a projectile";
         case "Ground_or_Air":
             candidateList = [
-                ["COLLISION_SITUATION_MASK_A", "hits opponents in air"],
-                ["COLLISION_SITUATION_MASK_G", "hits opponents on ground"],
-                ["COLLISION_SITUATION_MASK_GA", "hits opponents both in air and on ground"],
+                ["COLLISION_SITUATION_MASK_A", "Hits opponents in air"],
+                ["COLLISION_SITUATION_MASK_G", "Hits opponents on ground"],
+                ["COLLISION_SITUATION_MASK_GA", "Hits opponents both in air and on ground"],
+                ["COLLISION_SITUATION_MASK_G_d", "Hits opponents on ground but not downed"],
+                ["COLLISION_SITUATION_MASK_GA_d", "Hits opponents both in air and on ground but not downed"]
             ];
             return (
                 <span>
-                    Possible values are:
+                    Indicates if hitbox interacts with opponents in the ground and in the air
                     {DescribeCandidateList(candidateList, value)}
                 </span>);
         case "Hitbits":
             return "Value where each bit enables it to hit certain hurtboxes like characters, stage elements, items and enemies";
         case "CollisionPart":
-            return "no description";
+            return "Indicates which hurtboxes types hitbox can interact with (head, body, arms)";
         case "FriendlyFire":
-            return "Flag used for hitboxes that can hit user and teammates even with friendly fire enabled"; // "Team Damage" in Glossary
+            return "Flag used for hitboxes that can hit user and teammates even with friendly fire disabled";
         case "Effect":
-            return "no description";
+            candidateList = [
+                ['hash40("collision_attr_normal")', "Normal effect"],
+                ['hash40("collision_attr_fire")', "Fire effect"],
+                ['hash40("collision_attr_elec")', "Electric effect, multiplies hitlag x1.5"],
+                ['hash40("collision_attr_ice")', "Freezing effect, can freeze opponents"],
+                ['hash40("collision_attr_bury")', "Bury effect, opponents will be buried into the ground based on KB and opponent %"],
+                ['hash40("collision_attr_cutup")', "Slash effect"],
+                ['hash40("collision_attr_sting")', "Stab effect"],
+                ['hash40("collision_attr_magic")', "Magic effect"],
+                ['hash40("collision_attr_paralyze")', "Paralyze effect, opponents will be paralyzed based on hitlag and KB"],
+                ['hash40("collision_attr_purple")', "Darkness effect"],
+                ['hash40("collision_attr_search")', "Searchbox, used to detect opponents to initiate a move like Raptor Boost and Upperdash/Electroshock Arm"],
+                ['hash40("collision_attr_water")', "Water effect"],
+                ['hash40("collision_attr_aura")', "Aura effect"],
+                ['hash40("collision_attr_rush")', "Rapid effect, used on rapid jabs"],
+                ['hash40("collision_attr_ink_hit")', "Ink effect"],
+                ['hash40("collision_attr_turn")', "Reverse effect, turns opponents around (Cape)"],
+                ['hash40("collision_attr_mario_local_coin")', "Mario Coin effect, can generate yellow or purple coins visual effects"],
+                ['hash40("collision_attr_coin")', "Coin effect, generates yellow coin visual effect"],
+                ['hash40("collision_attr_curse_poison")', "Poison effect, only causes visual effect (Eiha/Eigahon)"],
+                ['hash40("collision_attr_dedede_hammer")', "Dedede Hammer effect, same as normal effect"],
+                ['hash40("collision_attr_whip")',"Whip effect, used Simon/Richter hitboxes"],
+                ['hash40("collision_attr_saving")',"Focus Attack effect"],
+                ['hash40("collision_attr_lay")',"Down effect, causes the opponent to lay down on the ground"],
+                ['hash40("collision_attr_taiyo_hit")',"Sun Salutation effect"],
+                ['hash40("collision_attr_bind")',"Stun effect"],
+                ['hash40("collision_attr_bind_extra")',"Stun effect used on Mewtwo's Disable"]
+
+            ];
+            return (
+                <span>
+                    Indicates the effect of the hitbox
+                    {DescribeOnlyCandidateValue(candidateList, value)}
+                </span>);
         case "SFXLevel":
             return "Loudness of the Sound Effect";
         case "SFXType":
             return (<span>
-                Sound Effect,<br />represents the ID of the sound file to use when hitbox connects
+                Sound Effect, represents the ID of the sound file to use when hitbox connects
             </span>);
         case "Type":
-            return "no description";
+            return "Indicates hitbox type, used for modifiers for spirit traits";
         default:
             return "no description";
     }
@@ -187,4 +234,12 @@ function DescribeCandidateList(candidateList, value) {
     // 'value' is the current value which will be highlighted in the description
     return candidateList.map(([candidate, description]) =>
         DescribeCandidate(candidate, description, value))
+}
+
+function DescribeOnlyCandidateValue(candidateList, value) {
+    let match = candidateList.find(([candidate, val]) => candidate === value);
+    if(match){
+        return DescribeCandidate(match[0],match[1], value);
+    }
+    return null;
 }
